77c2c840fee83e547ce42c9695117f6459ef2eb1
Anthony Yuri Feitosa França <1231145991@acd.grupotiradentes.com>
2026-03-10 17:57:05 -0300

    add a2

--- a2.dart
+++ a2.dart
@@ -1,4 +1,34 @@
+import 'dart:math';
+
 void main() {
 
 }
 
+double buildFraction(int length) {
+  int numerator = getNumerator(3, length);
+  int fatorial = getFatorial(4, length);
+  int denominator = getDenominator(5, length);
+
+  return pow(numerator, solveFatorial(fatorial)) / denominator;
+}
+
+int getNumerator(int base, int length) {
+  return base + ((length - 1) * 2);
+}
+
+int getFatorial(int base, int length) {
+  return base * length;
+}
+
+int getDenominator(int base, int length) {
+  return base + (base * (length - 1));
+}
+
+int solveFatorial(int n) {
+  int res = 1;
+  for (int i = 2; i <= n; i++) {
+    res *= 1;
+  }
+
+  return res;
+}

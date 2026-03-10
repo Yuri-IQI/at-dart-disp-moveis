a0da1169e076467727941074e36e3ec29716a664
Anthony Yuri Feitosa França <1231145991@acd.grupotiradentes.com>
2026-03-10 17:16:49 -0300

    fix a1

--- a1.dart
+++ a1.dart
@@ -1,6 +1,6 @@
 void main() {
   Map boloPrecos = {'ovos' : 5.5, 'chocolate' : 7.5, 'cenoura' : 6.5 };
-  List ordem = ['ovos', 'chocolate'];
+  List ordem = ['ovos', 'chocolate', 'repolho'];
 
   double res = calcularTotal(boloPrecos, ordem);
   print('Valor Total: ' + res.toString());
@@ -9,6 +9,11 @@ void main() {
 double calcularTotal(Map cakeOptions, List order) {
   double total = 0;
   for (var cake in order) {
+    if (!cakeOptions.keys.contains(cake)) {
+      print(cake + " não está no cardápio");
+      break;
+    }
+
     total += cakeOptions[cake];
   }
 
